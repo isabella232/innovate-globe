c07d5df877fbba3b90f0f4e30ee6e10e7c83d102
Olivier Lamothe <olamothe@coveo.com>
2022-10-31 09:25:10 -0400

    working state

---
+++ src/AnimatedGlobe.tsx
@@ -0,0 +1,208 @@
+import { FunctionComponent, useEffect, useRef, useState } from "react";
+import ReactGlobeGl, { GlobeMethods } from "react-globe.gl";
+
+interface ArcData {
+  filter?: any;
+  startLat: number;
+  startLng: number;
+  endLat: number;
+  endLng: number;
+  color: string;
+  timestamp: number;
+}
+
+interface RingData {
+  lat: number;
+  lng: number;
+  color: string;
+  timestamp: number;
+}
+
+interface AnimatedGlobeProps {
+  renderRings: boolean;
+  renderArcs: boolean;
+  anim: boolean;
+  autoRotate: boolean;
+  debug: boolean;
+  tickSpeed: number;
+  numRings: number;
+  flightTime: number;
+  arcRelativeLength: number;
+  ringRadius: number;
+  ringSpeed: number;
+  numberOfAnimation: number;
+  arcDashGap: number;
+  arcStroke: number;
+  earthImg: string;
+  atmosphereColor: string;
+  atmosphereAltitude: number;
+  arcAltitudeAutoScale: number;
+}
+
+export const AnimatedGlobe: FunctionComponent<AnimatedGlobeProps> = ({
+  renderRings,
+  renderArcs,
+  anim,
+  autoRotate,
+  debug,
+  tickSpeed,
+  numRings,
+  flightTime,
+  arcRelativeLength,
+  ringRadius,
+  ringSpeed,
+  numberOfAnimation,
+  arcDashGap,
+  earthImg,
+  arcStroke,
+  atmosphereColor,
+  atmosphereAltitude,
+  arcAltitudeAutoScale,
+}) => {
+  const [arcsData, setArcsData] = useState<ArcData[]>([]);
+  const [ringsData, setRingsData] = useState<RingData[]>([]);
+  const [animationTick, setAnimationTick] = useState(0);
+  const globeRef = useRef<GlobeMethods>();
+
+  const emitArc = async () => {
+    const datum = [...Array(numberOfAnimation).keys()].map(() => {
+      const startLat = (Math.random() - 0.5) * 180;
+      const startLng = (Math.random() - 0.5) * 360;
+      const endLat = 37.926868;
+      const endLng = -78.024902;
+      const color = `hsl(${360 * Math.random()}, ${25 + 70 * Math.random()}%,${
+        85 + 10 * Math.random()
+      }%`;
+
+      const arc = {
+        startLat,
+        startLng,
+        endLat,
+        endLng,
+        color,
+        timestamp: new Date().getTime(),
+      };
+      const sourceRing = {
+        lat: startLat,
+        lng: startLng,
+        color,
+        timestamp: new Date().getTime(),
+      };
+
+      return {
+        arc,
+        sourceRing,
+      };
+    });
+
+    const arcs = datum.map((d) => d.arc);
+    const sourceRings = datum.map((d) => d.sourceRing);
+    const evictionTimeForArcs = flightTime * 2;
+    const evictionTimeForRings = flightTime * arcRelativeLength;
+
+    setArcsData((arcsData: ArcData[]) => [...arcsData, ...arcs]);
+    setTimeout(
+      () =>
+        setArcsData((arcsDataToEvict: ArcData[]) => {
+          return arcsDataToEvict.filter((d) => {
+            return (
+              Math.abs(d.timestamp - new Date().getTime()) <=
+              evictionTimeForArcs
+            );
+          });
+        }),
+      evictionTimeForArcs
+    );
+
+    setRingsData((ringsData: RingData[]) => [...ringsData, ...sourceRings]);
+    setTimeout(
+      () =>
+        setRingsData((ringsDataToEvict: RingData[]) => {
+          return ringsDataToEvict.filter((d) => {
+            return (
+              Math.abs(d.timestamp - new Date().getTime()) <=
+              evictionTimeForRings
+            );
+          });
+        }),
+      evictionTimeForRings
+    );
+
+    setAnimationTick(animationTick + 1);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(async () => {
+      await emitArc();
+    }, tickSpeed);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [animationTick]);
+
+  if (globeRef.current) {
+    (globeRef.current.controls() as any).autoRotate = autoRotate;
+  }
+
+  if (!anim) {
+    globeRef.current?.pauseAnimation();
+  } else {
+    globeRef.current?.resumeAnimation();
+  }
+
+  return (
+    <ReactGlobeGl
+      labelsData={
+        debug
+          ? [
+              { lat: 0, lng: 0, text: `Tick: ${animationTick.toString()}` },
+              {
+                lat: 2,
+                lng: 0,
+                text: `Arcs: ${arcsData.length}`,
+              },
+              {
+                lat: 4,
+                lng: 0,
+                text: `Rings: ${ringsData.length}`,
+              },
+              {
+                lat: 6,
+                lng: 0,
+                text: `Memory: ${Math.round(
+                  (window.performance as any).memory.usedJSHeapSize / 1048576
+                )} MB / ${Math.round(
+                  (window.performance as any).memory.jsHeapSizeLimit / 1048576
+                )} MB`,
+              },
+            ]
+          : []
+      }
+      labelSize={() => 1}
+      labelResolution={2}
+      labelIncludeDot={false}
+      labelsTransitionDuration={0}
+      labelColor={() => "rgba(255, 165, 0, 0.75)"}
+      ref={globeRef}
+      arcsData={renderArcs ? arcsData : []}
+      arcColor={"color"}
+      arcDashLength={arcRelativeLength}
+      arcDashGap={arcDashGap}
+      arcDashInitialGap={1}
+      arcDashAnimateTime={flightTime}
+      arcsTransitionDuration={0}
+      arcAltitudeAutoScale={arcAltitudeAutoScale}
+      arcStroke={arcStroke === 1 ? null : arcStroke}
+      ringsData={renderRings ? ringsData : []}
+      ringColor={"color"}
+      ringResolution={100}
+      ringMaxRadius={ringRadius}
+      ringPropagationSpeed={ringSpeed}
+      ringRepeatPeriod={(flightTime * arcRelativeLength) / numRings}
+      backgroundImageUrl="/night-sky.png"
+      globeImageUrl={earthImg}
+      atmosphereColor={atmosphereColor}
+      atmosphereAltitude={atmosphereAltitude}
+    />
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,261 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { AnimatedGlobe } from "./AnimatedGlobe";
 
 function App() {
+  const [renderRings, setRenderRings] = useState(true);
+  const [renderArcs, setRenderArcs] = useState(true);
+  const [animate, setAnimate] = useState(true);
+  const [rotate, setRotate] = useState(true);
+  const [debug, setDebug] = useState(false);
+  const [tickSpeed, setTickSpeed] = useState(500);
+  const [numRings, setNumRings] = useState(1);
+  const [flightTime, setFlightTime] = useState(2000);
+  const [arcRelativeLength, setArcRelativeLength] = useState(0.5);
+  const [ringRadius, setRingRadius] = useState(3);
+  const [ringSpeed, setRingSpeed] = useState(5);
+  const [numberOfAnimation, setNumberOfAnimation] = useState(50);
+  const [arcDashGap, setArcDashGap] = useState(2);
+  const [arcStroke, setArcStroke] = useState(1);
+  const [arcAltitudeAutoScale, setArcAltitudeAutoScale] = useState(0.5);
+  const [atmosphereColor, setAtmosphereColor] = useState("rgb(255,255,255)");
+  const [atmosphereAltitude, setAtmosphereAltitude] = useState(0.2);
+  const [earthImg, setEarthImg] = useState(
+    "//unpkg.com/three-globe/example/img/earth-night.jpg"
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {new URL(document.location.href).searchParams.get("admin") === "1" && (
+        <div
+          style={{
+            position: "absolute",
+            top: 50,
+            left: 0,
+            padding: 10,
+            zIndex: 9999,
+            backgroundColor: "white",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div>
+            <label>
+              Render rings
+              <input
+                type="checkbox"
+                checked={renderRings}
+                onChange={() => setRenderRings(!renderRings)}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Render arcs
+              <input
+                type="checkbox"
+                checked={renderArcs}
+                onChange={() => setRenderArcs(!renderArcs)}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Animation
+              <input
+                type="checkbox"
+                checked={animate}
+                onChange={() => setAnimate(!animate)}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Auto rotate
+              <input
+                type="checkbox"
+                checked={rotate}
+                onChange={() => setRotate(!rotate)}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Debug
+              <input
+                type="checkbox"
+                checked={debug}
+                onChange={() => setDebug(!debug)}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Tick speed
+              <input
+                type="number"
+                value={tickSpeed}
+                onChange={(e) => setTickSpeed(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Number of animations
+              <input
+                type="number"
+                value={numberOfAnimation}
+                onChange={(e) => setNumberOfAnimation(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Flight time
+              <input
+                type="number"
+                value={flightTime}
+                onChange={(e) => setFlightTime(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Arc relative length
+              <input
+                type="text"
+                value={arcRelativeLength}
+                onChange={(e) => setArcRelativeLength(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Arc dash gap
+              <input
+                type="text"
+                value={arcDashGap}
+                onChange={(e) => setArcDashGap(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Arc stroke
+              <input
+                type="number"
+                value={arcStroke}
+                onChange={(e) => setArcStroke(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Arc altitude
+              <input
+                type="number"
+                value={arcAltitudeAutoScale}
+                onChange={(e) =>
+                  setArcAltitudeAutoScale(Number(e.target.value))
+                }
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Ring radius
+              <input
+                type="text"
+                value={ringRadius}
+                onChange={(e) => setRingRadius(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Ring speed
+              <input
+                type="text"
+                value={ringSpeed}
+                onChange={(e) => setRingSpeed(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Number of rings
+              <input
+                type="number"
+                value={numRings}
+                onChange={(e) => setNumRings(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Atmosphere color
+              <input
+                type="color"
+                value={atmosphereColor}
+                onChange={(e) => setAtmosphereColor(e.target.value)}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Atmosphere altitude
+              <input
+                type="text"
+                value={atmosphereAltitude}
+                onChange={(e) => setAtmosphereAltitude(Number(e.target.value))}
+              ></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Earth
+              <select onChange={(e) => setEarthImg(e.target.value)}>
+                <option value="//unpkg.com/three-globe/example/img/earth-day.jpg">
+                  Day
+                </option>
+                <option value="//unpkg.com/three-globe/example/img/earth-night.jpg">
+                  Night
+                </option>
+                <option value="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg">
+                  Blue marble
+                </option>
+                <option value="//unpkg.com/three-globe/example/img/earth-dark.jpg">
+                  Dark
+                </option>
+                <option value="//unpkg.com/three-globe/example/img/earth-topology.png">
+                  Topology
+                </option>
+                <option value="//unpkg.com/three-globe/example/img/earth-water.png">
+                  Water
+                </option>
+              </select>
+            </label>
+          </div>
+        </div>
+      )}
+
+      <AnimatedGlobe
+        autoRotate={rotate}
+        renderRings={renderRings}
+        renderArcs={renderArcs}
+        anim={animate}
+        debug={debug}
+        tickSpeed={tickSpeed}
+        numRings={numRings}
+        flightTime={flightTime}
+        arcRelativeLength={arcRelativeLength}
+        ringRadius={ringRadius}
+        ringSpeed={ringSpeed}
+        numberOfAnimation={numberOfAnimation}
+        arcDashGap={arcDashGap}
+        earthImg={earthImg}
+        arcStroke={arcStroke}
+        atmosphereColor={atmosphereColor}
+        atmosphereAltitude={atmosphereAltitude}
+        arcAltitudeAutoScale={arcAltitudeAutoScale}
+      />
     </div>
   );
 }
