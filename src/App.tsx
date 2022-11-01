51aecb00071d6f1546153f21362a7b109ee7968f
mpayne <mpayne@coveo.com>
2022-11-01 15:07:52 -0400

    feat: added easter egg

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,29 @@ import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
 import { GlobeAndPanel } from "./GlobeAndPanel";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Charts } from "./Charts";
+import Konami from 'react-konami-code';
+import gif from './dancing-skelly.gif';
 
 function App() {
+  function easterEgg() {
+    var imgDiv = document.createElement("div");
+    imgDiv.style.position = "fixed";
+    imgDiv.style.zIndex = "2";
+    imgDiv.style.bottom = "40%";
+    imgDiv.style.padding = "10px";
+    imgDiv.style.left = "230%";
+    imgDiv.style.width = "200%";
+    imgDiv.style.transform = "scale(3)";
+
+    var img = document.createElement("img");
+    img.src = gif;
+    imgDiv.appendChild(img);
+
+    let root = document.getElementById("root")
+    if (root != null) {
+      root.appendChild(imgDiv);
+    }
+  }
   return (
     <BrowserRouter>
       <QueryParamProvider adapter={ReactRouter6Adapter}>
@@ -13,7 +34,11 @@ function App() {
             path="/"
             element={
               <>
-                <GlobeAndPanel /> <Charts />
+                <GlobeAndPanel /> 
+                <Charts />
+                <Konami action={easterEgg}>
+                  {"Hey, I'm an Easter Egg! Look at me!"}
+                </Konami>
               </>
             }
           />
