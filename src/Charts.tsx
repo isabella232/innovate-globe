b22083c61f83dc03dbcfe2044e8ff7ec295c8e8f
Olivier Lamothe <olamothe@coveo.com>
2022-11-07 13:24:28 -0500

    change wording

--- src/Charts.tsx
+++ src/Charts.tsx
@@ -156,13 +156,13 @@ export const Charts: FunctionComponent<ChartsProps> = ({
     currency: "USD",
   });
 
-  const qpsUs = numEventsUs.length
+  const eventsPerSecondsUS = numEventsUs.length
     ? numEventsUs[numEventsUs.length - 1].num
     : 0;
-  const qpsEu = numEventsEu.length
+  const eventsPerSecondsEu = numEventsEu.length
     ? numEventsEu[numEventsEu.length - 1].num
     : 0;
-  const qpsAu = numEventsAu.length
+  const eventsPerSecondsAu = numEventsAu.length
     ? numEventsAu[numEventsAu.length - 1].num
     : 0;
 
@@ -228,22 +228,22 @@ export const Charts: FunctionComponent<ChartsProps> = ({
             {formatter.format(money)}
           </Text>
           <Text size="xl" color="white" weight="bold">
-            QPS (us-east-1):
+            Events per seconds (us-east-1):
           </Text>
-          <Text size={30} color={qpsUs === 0 ? "grey" : "green"}>
-            {qpsUs.toString()}
-          </Text>
-          <Text size="xl" color="white" weight="bold">
-            QPS (eu-west-1):
-          </Text>
-          <Text size={30} color={qpsEu === 0 ? "grey" : "green"}>
-            {qpsEu.toString()}
+          <Text size={30} color={eventsPerSecondsUS === 0 ? "grey" : "green"}>
+            {eventsPerSecondsUS.toString()}
           </Text>
           <Text size="xl" color="white" weight="bold">
-            QPS (ap-southeast-2):
+            Events per seconds (eu-west-1):
           </Text>
-          <Text size={30} color={qpsAu === 0 ? "grey" : "green"}>
-            {qpsAu.toString()}
+          <Text size={30} color={eventsPerSecondsEu === 0 ? "grey" : "green"}>
+            {eventsPerSecondsEu.toString()}
+          </Text>
+          <Text size="xl" color="white" weight="bold">
+            Events per seconds (ap-southeast-2):
+          </Text>
+          <Text size={30} color={eventsPerSecondsAu === 0 ? "grey" : "green"}>
+            {eventsPerSecondsAu.toString()}
           </Text>
 
           <Pie
